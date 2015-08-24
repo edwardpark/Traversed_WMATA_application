84f4bafe5a4630eecf0f26f5a4f0b7fbcdd8cceb
Edward Park <parkes.edward@gmail.com>
2015-08-24 14:58:04 -0400

    connected wmta api

--- app.js
+++ app.js
@@ -1,6 +1,24 @@
 var express = require('express');
 var app = express();
 
+var request = require("request");
+
+var configuration = require("./config/keys.json");
+var wmta_key = configuration.wmta.api_key;
+
 app.listen("3000", function(){
-  console.log("bug Burrito is SAUCY!")
+  console.log("big Burrito is SAUCY!")
 });
+var stopId = 1001195;
+var apiKey = wmta_key;
+
+var options = {
+  url: 'https://api.wmata.com/NextBusService.svc/json/jPredictions?StopID=' + stopId + '&api_key='+ apiKey,
+};
+function callback(error, response, body) {
+  if (!error && response.statusCode == 200) {
+    console.log([body]);
+  }
+}
+
+request(options, callback);
