65e665733c135ab1e61768d4dfc95818ac3a7f17
Erica Andersen <andersenek@gmail.com>
2015-08-27 15:53:21 -0400

    Full database of bus stops, Fixed user match to response

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 var express = require('express');
 var app = express();
 var path = require("path");
+
 var bodyParser = require("body-parser");
 app.use(bodyParser.json())
+
 app.use("/public", express.static(path.join(__dirname + "/public")));
 app.set("view engine", "hbs");
+
 var busStopsController = require("./controllers/busStops");
 
 app.get("/", function(req, res){
@@ -16,6 +19,7 @@ app.get("/about", function(req, res){
 app.use("/", busStopsController);
 
 var request = require("request");
+
 ///security configuration for api keys////
 var configuration = require("./config/keys.json");
 var wmta_key = configuration.wmta.api_key;
@@ -23,8 +27,8 @@ var darkSky_key = configuration.darkSky.api_key;
 app.listen("3000", function(){
   console.log("big Burrito is SAUCY!")
 });
+
 /////////////////////NextBus API call///////////////////////
-//var stopId = 1001195;
 var apiKey = wmta_key;
 var darkSkyApiKey = darkSky_key;
 var latitude;
@@ -56,7 +60,6 @@ app.get("/busstop/:id", function(req, nodeResponse){
       getBusInfo.rez = nodeResponse; // nodeResponse will become the response
       getBusInfo.busAPIInfo = JSON.parse(body);
       getBusInfo.sendJSON();
-      console.log("app.js stored stop id =" + getBusInfo.id)
     }
   });//end of request module
 });
